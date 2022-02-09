a402f8b00328ba0d8a1b3c436a652b02207bf165
aswever <aswever@protonmail.com>
2022-02-09 13:49:25 -0500

    add redux-persist

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,15 +1,35 @@
-import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import {
+  configureStore,
+  ThunkAction,
+  Action,
+  combineReducers,
+} from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import counterReducer from "../features/counter/counterSlice";
 import accountsReducer from "../features/accounts/accountsSlice";
 import contractsReducer from "../features/contracts/contractsSlice";
 
-export const store = configureStore({
-  reducer: {
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const reducer = persistReducer(
+  persistConfig,
+  combineReducers({
     counter: counterReducer,
     accounts: accountsReducer,
     contracts: contractsReducer,
-  },
+  })
+);
+
+export const store = configureStore({
+  reducer,
+  middleware: (mw) => mw({ serializableCheck: false }),
 });
+
+export const persistor = persistStore(store);
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { store } from './app/store';
-import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { persistor, store } from "./app/store";
+import { Provider } from "react-redux";
+import * as serviceWorker from "./serviceWorker";
+import { PersistGate } from "redux-persist/es/integration/react";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
