a3e57694bf657e5f4d3bc956e904a34f64188624
aswever <aswever@protonmail.com>
2022-06-05 14:35:01 +0000

    pretty json output viewer

--- src/features/console/Output.tsx
+++ src/features/console/Output.tsx
@@ -1,11 +1,17 @@
-import React, { FC } from "react";
-import formatHighlight from "json-format-highlight";
+import React, { FC, useMemo } from "react";
 import { useAppSelector } from "../../app/hooks";
 import styles from "./Output.module.css";
-import { highlightColors } from "./consoleSlice";
+import ReactJson from "react-json-view";
 
 export const Output: FC = () => {
   const output = useAppSelector((state) => state.console.output);
+  const outputObject = useMemo(() => {
+    try {
+      return JSON.parse(output);
+    } catch (_) {
+      return;
+    }
+  }, [output]);
   const error = useAppSelector((state) => state.console.error);
 
   const classes = [styles.output];
@@ -15,11 +21,11 @@ export const Output: FC = () => {
 
   return (
     <div className={classes.join(" ")}>
-      <pre
-        dangerouslySetInnerHTML={{
-          __html: error ? output : formatHighlight(output, highlightColors),
-        }}
-      />
+      {outputObject ? (
+        <ReactJson src={outputObject} indentWidth={2} quotesOnKeys={false} />
+      ) : (
+        <pre dangerouslySetInnerHTML={{ __html: output }} />
+      )}
     </div>
   );
 };
